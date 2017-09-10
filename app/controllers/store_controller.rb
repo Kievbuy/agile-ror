e55dd2e79e3b17d8d2cdc594a657d5873a469949
kievbuy <kievbuy@gmail.com>
2017-09-10 08:29:40 +0000

    Add time to footer

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,9 @@ class StoreController < ApplicationController
   
   def index
     @products = Product.order(:title)
+    
+    @time = Time.new.localtime("+03:00").strftime("%d.%m.%Y")
+  
   end
   
 end

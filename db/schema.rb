fe1710bd7e46f9b9f1d0502b32395ea9be053ffe
kievbuy <kievbuy@gmail.com>
2017-09-10 16:51:54 +0000

    Migrate cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909091742) do
+ActiveRecord::Schema.define(version: 20170910164707) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
